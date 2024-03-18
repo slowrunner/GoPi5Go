8067fd7e6bb69b6354d1a7ee0f0877a3ea70b97e
slowrunner <slowrunner@users.noreply.github.com>
2024-03-17 20:15:41 -0400

    init

---
+++ utils/monitor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+while true; \
+do echo -e "\n********** GoPi5Go  MONITOR ******************************"; \
+echo -n `date +"%A %D"`; \
+# echo -e '\n'; \
+uptime; \
+vcgencmd measure_temp && vcgencmd measure_clock arm && vcgencmd get_throttled; \
+free -h; \
+if [  -f /usr/bin/docker ]; then docker ps;  fi; \
+echo -e "\n"; \
+pgrep -a ros
+sleep 10; \
+echo " "; \
+done
