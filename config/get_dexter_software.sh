c337f371d3d5b9d5e6b8749de843c83cd1018150
slowrunner <slowrunner@users.noreply.github.com>
2024-03-20 09:26:37 -0400

    copy serials pkl file into place

--- config/get_dexter_software.sh
+++ config/get_dexter_software.sh
@@ -6,4 +6,5 @@ echo -e "Get Dexter/DI_Sensors/"
 git clone https://github.com/DexterInd/DI_Sensors.git /home/pi/Dexter/DI_Sensors
 echo -e "Get Dexter/RFR_Tools/"
 git clone https://github.com/DexterInd/RFR_Tools.git /home/pi/Dexter/lib/Dexter/RFR_Tools
-
+cp /home/pi/Dexter/GoPiGo3/Install/list_of_serial_numbers.pkl /home/pi/Dexter/.list_of_serial_numbers.pkl
+}
