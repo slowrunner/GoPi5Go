137d438f611675695ada97c8d2826801b2f88b5f
slowrunner <slowrunner@users.noreply.github.com>
2024-04-14 13:45:29 -0400

    fixed path GoPi5Go

--- config/oak-camera/4_Install_DepthAI.sh
+++ config/oak-camera/4_Install_DepthAI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/pi5desk/systests/vdepthai/
+cd ~/GoPi5Go/systests/Oak-D-W-97/vdepthai/
 git clone https://github.com/luxonis/depthai.git
 source bin/activate
 pip3 install opencv-python opencv-contrib-python
