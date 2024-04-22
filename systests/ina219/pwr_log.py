7aadd78e859ae24650021a8fa28addb7f108cfe4
slowrunner <slowrunner@users.noreply.github.com>
2024-04-22 08:31:38 -0400

    average v c p

--- systests/ina219/pwr_log.py
+++ systests/ina219/pwr_log.py
@@ -4,6 +4,7 @@ import ina219
 from ina219 import DeviceRangeError
 import time
 import logging
+import statistics
 
 SHUNT_OHMS = 0.1
 MAX_EXPECTED_AMPS = 2.0
@@ -21,6 +22,31 @@ mAh_meter = 0
 
 rate_per_hour = 20
 
+def ave_voltage():
+        vlist = []
+        for i in range(3):
+            vBatt = ina.voltage()
+            vlist += [vBatt]
+            time.sleep(0.01)  # cannot be faster than 0.005
+        return statistics.mean(vlist)
+
+def ave_current():
+        clist = []
+        for i in range(3):
+            cBatt = ina.current()
+            clist += [cBatt]
+            time.sleep(0.01)  # cannot be faster than 0.005
+        return statistics.mean(clist)
+
+def ave_power():
+        plist = []
+        for i in range(3):
+            pBatt = ina.power()
+            plist += [pBatt]
+            time.sleep(0.01)  # cannot be faster than 0.005
+        return statistics.mean(plist)/1000.0
+
+
 try:
     current_now = ina.current()
     if (current_now < 0):
@@ -30,9 +56,9 @@ try:
     last_charging = charging
 
     while True:
-        current_now = ina.current()
-        voltage_now = ina.voltage()
-        power_now = ina.power()
+        current_now = ave_current()
+        voltage_now = ave_voltage()
+        power_now = ave_power()
         if (current_now < 0):
             charging = True
         else:
@@ -48,9 +74,9 @@ try:
             power_meter = 0
             last_charging = charging
         mAh_meter += current_now / rate_per_hour 
-        power_meter += power_now / rate_per_hour / 1000.0
+        power_meter += power_now / rate_per_hour
 
-        print("{} Reading: {:.2f} V  {:.3f} A  {:.2f} W  {:.0f} mAh  {:.1f}Wh     ".format(tnow,ina.voltage(), ina.current()/1000.0, ina.power()/1000.0,mAh_meter,power_meter))
+        print("{} Reading: {:.2f} V  {:.3f} A  {:.2f} W  {:.0f} mAh  {:.1f}Wh     ".format(tnow,ave_voltage(), ave_current()/1000.0, ave_power(),mAh_meter,power_meter))
         time.sleep(3600/rate_per_hour)
 except DeviceRangeError as e:
     print(e)
