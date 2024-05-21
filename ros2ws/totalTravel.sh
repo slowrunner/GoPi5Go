e853ed3e09f472145ce13bb3bea92a3b140701da
slowrunner <slowrunner@users.noreply.github.com>
2024-05-21 14:47:42 -0400

    add tail odom log before total

--- ros2ws/totalTravel.sh
+++ ros2ws/totalTravel.sh
@@ -2,6 +2,9 @@
 
 logfile="/home/pi/GoPi5Go/logs/odometer.log"
 
+echo -e "\n*** TAIL ODOMETER.LOG ***"
+tail $logfile
+
 echo -e "\n*** TRAVEL FROM ODOMETRY.LOG ***"
 totalMoved=`(awk -F'moved:' '{sum+=$2}END{printf "%.1f", sum;}' $logfile)`
 totalMovedFt=`(echo "scale=1; ($totalMoved / 0.3048)" | bc)`
