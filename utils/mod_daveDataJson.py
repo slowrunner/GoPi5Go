a56772d7252951d8fdbbd1149b9d6477e77c44ed
slowrunner <slowrunner@users.noreply.github.com>
2024-06-12 08:55:57 -0400

    updates

---
+++ utils/mod_daveDataJson.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env  python3
+
+import sys
+sys.path.insert(1,"/home/pi/GoPi5Go/plib")
+
+import daveDataJson
+
+# print("daveDataJson contents:")
+# lcarlData = carlDataJson.getCarlData()
+# for i in lcarlData:
+#     print("  ",i," : ",lcarlData[i])
+
+daveDataJson.printData()
+ldaveData = daveDataJson.getData()
+for key in ldaveData:
+    value = ldaveData[key]
+    print("\nkey:",key,": ",value)
+    prompt = "Keep value \"{}\" y/n? ".format(value)
+    if input(prompt) == "n": 
+        while True:
+            val = input("Enter New Value: ")
+            prompt = "Use value \"{}\" y/n? ".format(val)
+            if input(prompt) == "y": break
+
+
+        if (daveDataJson.saveData(key, val) == True):
+            print('   Saved {}: {}'.format(key,val))
+        else:
+            print("   saveData({}) failed".format(key))
+
+print("\n")
+daveDataJson.printData()
---
+++ utils/nohup_test_docking.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+nohup /home/pi/GoPi5Go/systests/docking/test_docking.py  >/dev/null 2>&1 &
+
