04ac092f84f03c1aaf4a572d2dd29d4ff111d5a4
slowrunner <slowrunner@users.noreply.github.com>
2024-08-31 18:46:55 -0400

    ROS checks

--- ros2ws/check_battery.sh
+++ ros2ws/check_battery.sh
@@ -4,8 +4,10 @@ echo -e "\n*** Checking Battery with GoPiGo3 API"
 # python3 /home/pi/GoPi5Go/ros2ws/gopigo3_battery.py
 python3 gopigo3_battery.py
 
-echo -e "\n*** Checking Battery with ROS2"
-echo "ros2 topic echo --once /battery_state"
-ros2 topic echo --once /battery_state
+if [ ! -f /usr/bin/docker ]; then
+    echo -e "\n*** Checking Battery with ROS2"; \
+    echo "ros2 topic echo --once /battery_state"; \
+    ros2 topic echo --once /battery_state; \
+fi
 
 python3 /home/pi/GoPi5Go/plib/battery.py
---
+++ ros2ws/cmds/enumerate_joy_devices.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ros2 run joy joy_enumerate_devices
