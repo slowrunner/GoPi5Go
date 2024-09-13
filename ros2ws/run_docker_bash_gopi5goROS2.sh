43dfc429cca35bc26293f37129afdac9ffa815ad
slowrunner <slowrunner@users.noreply.github.com>
2024-09-12 22:47:18 -0400

    convenience files

---
+++ ros2ws/run_docker_bash_gopi5goROS2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# FILE:  run_docker_bash_gopi5goROS2bash.sh
+
+
+cd /home/pi/GoPi5Go/ros2ws
+# --rm \    remove container after running
+# Prob not need
+# --device /dev/gpiochip4 \
+
+docker run -it --net=host \
+ -v /dev/snd:/dev/snd \
+ -v /dev/input:/dev/input \
+ -v /home/pi:/home/pi \
+ -v /dev/bus/usb:/dev/bus/usb \
+ -v /dev/ttyUSB0:/dev/ttyUSB0 \
+ -v /var/lock:/var/lock \
+ -e TZ=America/New_York \
+ -w /home/pi/GoPi5Go/ros2ws \
+ --privileged \
+ --rm \
+ --name gopi5goROS2bash \
+ gopi5gor2hdp bash
---
+++ utils/replace_string_in_multiple_files.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+echo -e "To replace a string in all files in a dir:"
+echo -e "cd into dir"
+echo -e "sed -i .bak 's/foo/bar/g' *"
+echo -e "skip the .bak if no backup desired"
